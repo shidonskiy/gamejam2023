b91ad531a8d79dec5d0ced0f8472dfd2d3b043bf
Ruslan Krasikov <ruslan.krasikov@tensquaregames.com>
2023-10-13 14:38:22 +0200

    scale by target

--- Gamejam2023/Assets/Gamejam/Scripts/Controllers/Scripts/Environment/Collectable/Collector.cs
+++ Gamejam2023/Assets/Gamejam/Scripts/Controllers/Scripts/Environment/Collectable/Collector.cs
@@ -4,9 +4,13 @@ namespace Gamejam.Scripts.Controllers.Scripts.Environment.Collectable
 {
     public class Collector : MonoBehaviour
     {
+        public int Count => counter;
+        
+        private int counter = 0;
+        
         public void Claim(CollectableTypes collectable)
         {
-            
+            counter++;
         }
     }
 }
