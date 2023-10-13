4c7e44ab578c39b17b7e567475583bfc06f6e5c8
Ruslan Krasikov <ruslan.krasikov@tensquaregames.com>
2023-10-13 13:35:26 +0200

    add back to default

--- Gamejam2023/Assets/Gamejam/Scripts/Controllers/Scripts/Environment/BaseInteractable.cs
+++ Gamejam2023/Assets/Gamejam/Scripts/Controllers/Scripts/Environment/BaseInteractable.cs
@@ -4,9 +4,16 @@ namespace Gamejam.Scripts.Controllers.Scripts.Environment
 {
     public class BaseInteractable : MonoBehaviour
     {
+        protected bool backToDefault = false;
+        
         public virtual void OnToggle(bool isActive)
         {
-            
+            backToDefault = false;
+        }
+
+        public virtual void BackToDefault()
+        {
+            backToDefault = true;
         }
     }
 }
